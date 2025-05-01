2d4d7658ac7f0664df6705b4256ffb058d419ecd
YahyaNawabi <yahiaanawabi86@gmail.com>
2025-05-01 22:35:17 +0430

    Calculator

---
+++ Calculator.js
@@ -0,0 +1,17 @@
+// script.js
+function input(value) {
+    document.getElementById('display').textContent += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').textContent = '';
+  }
+  
+  function calculate() {
+    try {
+      const result = eval(document.getElementById('display').textContent);
+      document.getElementById('display').textContent = result;
+    } catch (e) {
+      document.getElementById('display').textContent = 'Error';
+    }
+  }
